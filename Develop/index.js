6702f0e5fc92c426f05e2525204e55618dd51521
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2022-11-21 18:31:39 +0000

    added object to questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,22 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 
+// inquirer.prompt([{
+//     type: "input",
+//     name: "name",
+//     prompt: "What is your name?"
+// }]),
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    name: "title",
+    prompt: "What is your project name?"
+}];
+
+const promptQuestions = questions.map(({type, name, prompt}) => ({prompt}))
+
+console.log(promptQuestions)
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
